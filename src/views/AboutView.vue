2aaa345b63b2074637ae68459365af0538c69925
Gift <giftgichuhi4138@gmail.com>
2024-03-13 12:55:33 +0300

    vue project able to interact with firebase

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,31 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button @click="execute">Execute</button>
   </div>
 </template>
+
+
+<script>
+
+import axios from 'axios';
+
+export default{
+  data(){
+    return{
+      vote:0
+    }
+  },
+  methods: {
+    async execute(){
+  try {
+    const response = await axios.get('/execute'); // Changed from POST to GET
+    console.log(response.data);
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
+
+  },
+};
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,84 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+   
+    <h1>signUp</h1>
+    <p><input type="text" placeholder="email" v-model="email"/></p>
+    <p><input type="password" placeholder="Password" v-model="password"/></p>
+    <button @click="signUp">Sign up</button>
+    <p>{{ crederror }}</p>
+    
+    <button @click="login">Login</button>
   </div>
+  
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref } from "vue";
+import { getAuth, createUserWithEmailAndPassword ,signInWithEmailAndPassword} from "firebase/auth";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCIjWYLSIkSghQ1T6zGnVyJ_8U-H9xVI0M",
+  authDomain: "testing2-eb9c5.firebaseapp.com",
+  projectId: "testing2-eb9c5",
+  storageBucket: "testing2-eb9c5.appspot.com",
+  messagingSenderId: "329757961053",
+  appId: "1:329757961053:web:c220873eb798bb3c3696b4"
+};
+
+// Initialize Firebase
+const firebase = initializeApp(firebaseConfig);
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
-  }
-}
+  data() {
+    return {
+      email: ref(""),
+      password: ref(""),
+      crederror: ref("")
+    };
+  },
+  methods: {
+    async signUp() {
+      try {
+        const auth = getAuth(firebase);
+        const userCredential = await createUserWithEmailAndPassword(auth, this.email, this.password);
+        const user = userCredential.user;
+        console.log("Successful registration");
+        console.log("New user:", user);
+        this.$router.push('/about') 
+      }  catch (error) {
+        console.error("error->",error.message); 
+        this.crederror=error.message;
+        // Capture the error parameter here
+      }
+    },
+    async login() {
+      console.log("midway");
+      try {
+        const auth = getAuth(firebase); // Corrected the variable name
+        const userCredential = await signInWithEmailAndPassword(auth, this.email, this.password);
+        const user = userCredential.user;
+        console.log("Successful logged in");
+        console.log("Logged in user:", user);
+        this.$router.push('/about');
+      } catch (error) {
+        console.error("error->",error.message);
+        this.crederror=error.message;
+      }
+    }
+  },
+ 
+  
+  };
 </script>
+
+
+
+
+
